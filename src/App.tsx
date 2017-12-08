a0a7eb61f18d16d4b6ba2b846824d57f8441fc6d
ncphillips <ncphillips.19@gmail.com>
2017-12-08 18:48:28 -0400

    chore(heading): Updated heading to say paper-trails.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-const logo = require('./logo.svg');
+const logo = require("./logo.svg");
 
 class App extends React.Component {
   render() {
@@ -9,7 +9,7 @@ class App extends React.Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Paper Games</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
